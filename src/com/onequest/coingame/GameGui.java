63254ac4445523c821cb2d562ed2940cb6c6aff1
VogelDev <rvogel@mc3.edu>
2014-12-12 12:31:53 -0500

    Computer Play
    Started coding the computer player for the GUI game.

--- src/com/onequest/coingame/GameGui.java
+++ src/com/onequest/coingame/GameGui.java
@@ -20,6 +20,8 @@ public class GameGui extends JFrame {
     private static final long serialVersionUID = 1L;
     private JPanel contentPane;
     private Board board;
+    private Player player;
+    private Move move;
 
     /**
      * Launch the application.
@@ -48,6 +50,7 @@ public class GameGui extends JFrame {
         contentPane.setLayout(new BorderLayout(0, 0));
         setContentPane(contentPane);
         board = new Board();
+        player = new Player();
 
         JButton btnNewButton = new JButton(board.get(0).orient);
         btnNewButton.addActionListener(new ActionListener() {
@@ -57,8 +60,8 @@ public class GameGui extends JFrame {
                     btnNewButton.setText(board.get(0).orient);
                 } catch (Exception e) {
                     JOptionPane.showMessageDialog(null, // position
-                            "Error", // title
                             e.getMessage(), // message
+                            "Error", // title
                             JOptionPane.ERROR_MESSAGE // icon
                             );
                 }
@@ -75,8 +78,8 @@ public class GameGui extends JFrame {
                     btnNewButton_1.setText(board.get(1).orient);
                 } catch (Exception e) {
                     JOptionPane.showMessageDialog(null, // position
-                            "Error", // title
                             e.getMessage(), // message
+                            "Error", // title
                             JOptionPane.ERROR_MESSAGE // icon
                             );
                 }
@@ -93,8 +96,8 @@ public class GameGui extends JFrame {
                     btnNewButton_2.setText(board.get(2).orient);
                 } catch (Exception e) {
                     JOptionPane.showMessageDialog(null, // position
-                            "Error", // title
                             e.getMessage(), // message
+                            "Error", // title
                             JOptionPane.ERROR_MESSAGE // icon
                             );
                 }
@@ -105,6 +108,28 @@ public class GameGui extends JFrame {
         JButton btnNewButton_3 = new JButton("Computer Play");
         btnNewButton_3.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) {
+                Board tmp;
+                while (!board.isOver()) {
+                    int choice = player.takeTurn(board);
+                    try {
+                        tmp = new Board(board.get(0).getFacing(), board.get(1).getFacing(), board.get(2).getFacing());
+                        board.flip(choice);
+
+                        JOptionPane.showMessageDialog(null, String.valueOf(choice + 1));
+                        move = new Move(tmp, choice, player.getCurrentSet().moveCount, true);
+                        player.getCurrentSet().add(move);
+                        //System.out.println(player.getCurrentSet());
+                    } catch (Exception e) {
+                        //System.out.println("failed");
+                        //System.out.println(board + ", " + choice);
+                        endGame(choice, false);
+                        break;
+                    }
+                    if (board.isOver()) {
+                        endGame(choice, true);
+                        break;
+                    }
+                }
             }
         });
         contentPane.add(btnNewButton_3, BorderLayout.SOUTH);
@@ -119,6 +144,25 @@ public class GameGui extends JFrame {
             }
         });
         contentPane.add(btnNewGame, BorderLayout.NORTH);
+    }
+    
+    private void endGame(int choice, boolean legal) {
+        move = new Move(board, choice, player.getCurrentSet().moveCount, legal);
+        player.getCurrentSet().add(move);
+        player.getCurrentSet().setSuccess(legal);
+        //System.out.println(move);
+        if (legal){
+            if (player.getCurrentSet().export())
+                System.out.println("Exported");
+            
+            System.out.println(player.getCurrentSet().moveCount);
+        }else{
+            player.getIllegal().add(move);
+            MoveSet.export(player.getIllegal());
+        }
+
+        player = new Player();
+        board = new Board();
     }
 
 }
